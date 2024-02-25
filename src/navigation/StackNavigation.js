b779fe9b209a8cddcd314105c4886a82d027f689
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-02-25 14:39:31 +0700

    use bottomtab navigation

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -1,38 +1,98 @@
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DiscountScreen from "../screens/discount/DiscountScreen";
 import HomeScreen from "../screens/home/HomeScreen";
 import ProfileScreen from "../screens/profile/ProfileScreen";
-import TheaterScreen from "../screens/theater/TheaterScreen";
+import TheatersScreen from "../screens/theater/TheaterScreen";
 
+const HomeStack = createNativeStackNavigator();
+const TheaterStack = createNativeStackNavigator();
+const DiscountStack = createNativeStackNavigator();
+const ProfileStack = createNativeStackNavigator();
+
+const HomeStackScreens = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Trang chủ"
+        component={HomeScreen}
+        options={{ title: "" }}
+      />
+    </HomeStack.Navigator>
+  );
+};
+
+const TheaterStackScreens = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Chọn rạp" component={TheatersScreen} />
+    </HomeStack.Navigator>
+  );
+};
+
+const DiscountStackScreens = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Khuyến mãi" component={DiscountScreen} />
+    </HomeStack.Navigator>
+  );
+};
+
+const ProfileStackScreens = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Tài khoản" component={ProfileScreen} />
+    </HomeStack.Navigator>
+  );
+};
 const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
+  // const getHeaderTitle = (route) => {
+  //   const routeName = route.state
+  //     ? route.state.routes[route.state.index].name
+  //     : route.params?.screen || "Trang chủ";
+
+  //   // Trả về tiêu đề tương ứng với tên màn hình
+  //   switch (routeName) {
+  //     case "Trang chủ":
+  //       return "";
+  //     case "Chọn rạp":
+  //       return "Chọn rạp";
+  //     case "Khuyến mãi":
+  //       return "Khuyến mãi";
+  //     case "Tài khoản":
+  //       return "Tài khoản";
+  //   }
+  // };
+
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName="Home"
+        initialRouteName="Trang chủ"
         screenOptions={{
           tabBarActiveTintColor: "#FE4A4B",
+          tabBarHideOnKeyboard: true,
         }}
       >
         <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: "Home",
+          name="Trang chủ"
+          component={HomeStackScreens}
+          options={({ route }) => ({
+            headerShown: false,
             tabBarLabelStyle: { color: "black" },
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
             ),
-          }}
+          })}
         />
         <Tab.Screen
-          name="Theater"
-          component={TheaterScreen}
-          options={{
-            tabBarLabel: "Theater",
+          name="Chọn rạp"
+          component={TheaterStackScreens}
+          options={({ route }) => ({
+            headerShown: false,
             tabBarLabelStyle: { color: "black" },
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons
@@ -41,24 +101,28 @@ const Navigation = () => {
                 color={color}
               />
             ),
-          }}
+          })}
         />
         <Tab.Screen
-          name="Discount"
-          component={DiscountScreen}
+          name="Khuyến mãi"
+          component={DiscountStackScreens}
           options={{
-            tabBarLabel: "Discount",
+            headerShown: false,
+            tabBarLabel: "Khuyến mãi",
             tabBarLabelStyle: { color: "black" },
             tabBarIcon: ({ color, size }) => (
               <MaterialIcons name="discount" size={size} color={color} />
             ),
+            // load động
+            tabBarBadge: 4,
           }}
         />
         <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
+          name="Tài khoản"
+          component={ProfileStackScreens}
           options={{
-            tabBarLabel: "Profile",
+            headerShown: false,
+            tabBarLabel: "Tài khoản",
             tabBarLabelStyle: { color: "black" },
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons
